af91cef1df7dcf31dfb5ef95cbadf0438941fd9d
Bheemaraju Bojja <bheemaraju.bojja@lexyslabs.com>
2026-01-13 16:19:27 +0530

    commented the email sending code

--- src/controllers/v1/sendPasswordResetLink.ts
+++ src/controllers/v1/sendPasswordResetLink.ts
@@ -43,14 +43,14 @@ export const sendPasswordResetLink = async (req, res) => {
     const subject = 'Reset your password'
     let recipient_first_name = emailExistence?.name
     let hyperlink = `${config.FE_DOMAIN}/reset-password?email=${email}&hash=${hash}`;
-
+// console.log('hyperlink = ', hyperlink)
     const context = {
       logo,
       recipient_first_name,
       hyperlink
     }
-
-    await sendEmail({ toEmail: email, subject, template: 'passwordResetLink', context });
+    // Uncomment below line to make the email work.
+    // await sendEmail({ toEmail: email, subject, template: 'passwordResetLink', context });
 
     return res.status(200).json({
       success: true,
